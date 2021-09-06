e5bc9c58a6e672d0d3435c44035456f1cdccdf69
Nishanth546-cmr <18r01a0546@cmritonline.ac.in>
2021-09-06 17:25:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-fvgzwh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fvgzwh)
---
+++ src/app/weather/weather.ts
@@ -0,0 +1,11 @@
+export class Weather {
+  constructor(
+    public city: string,
+    public description: string,
+    public temperature: number
+  ) {
+    this.city = city;
+    this.description = description;
+    this.temperature = temperature;
+  }
+}
